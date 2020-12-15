73a9d3e54e5a973dc964b41f62d7c134e579e2e7
G-Schwarz <46079708+G-Schwarz@users.noreply.github.com>
2020-12-15 12:46:15 +0700

    frmMain undergo
    added icons, functional buttons, UC overview undergo

--- PharmacyManagement/FrmMain.Designer.cs
+++ PharmacyManagement/FrmMain.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PharmacyManagement
+﻿using System.Drawing;
+
+namespace PharmacyManagement
 {
     partial class FrmMain
     {
@@ -29,7 +31,8 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.panel2 = new System.Windows.Forms.Panel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.Pnl_Sidemenu = new System.Windows.Forms.Panel();
             this.Btn_Customers = new System.Windows.Forms.Button();
             this.Btn_Storage = new System.Windows.Forms.Button();
             this.Btn_Sale = new System.Windows.Forms.Button();
@@ -41,38 +44,42 @@
             this.uC_Sale1 = new PharmacyManagement.UC_Sale();
             this.uC_employee1 = new PharmacyManagement.UC_Employee();
             this.uC_overview1 = new PharmacyManagement.UC_Overview();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Lb_Name = new System.Windows.Forms.Label();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
-            this.panel2.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Pnl_Sidemenu.SuspendLayout();
             this.Pnl_Main.SuspendLayout();
-            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel2
+            // Pnl_Sidemenu
             // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
-            this.panel2.BackColor = System.Drawing.Color.SteelBlue;
-            this.panel2.Controls.Add(this.Btn_Customers);
-            this.panel2.Controls.Add(this.Btn_Storage);
-            this.panel2.Controls.Add(this.Btn_Sale);
-            this.panel2.Controls.Add(this.Btn_Employee);
-            this.panel2.Controls.Add(this.Btn_Overview);
-            this.panel2.Location = new System.Drawing.Point(0, 72);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(180, 566);
-            this.panel2.TabIndex = 1;
+            this.Pnl_Sidemenu.BackColor = System.Drawing.Color.SteelBlue;
+            this.Pnl_Sidemenu.Controls.Add(this.panel3);
+            this.Pnl_Sidemenu.Controls.Add(this.Btn_Customers);
+            this.Pnl_Sidemenu.Controls.Add(this.Btn_Storage);
+            this.Pnl_Sidemenu.Controls.Add(this.Btn_Sale);
+            this.Pnl_Sidemenu.Controls.Add(this.Btn_Employee);
+            this.Pnl_Sidemenu.Controls.Add(this.Btn_Overview);
+            this.Pnl_Sidemenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Pnl_Sidemenu.Location = new System.Drawing.Point(0, 0);
+            this.Pnl_Sidemenu.Name = "Pnl_Sidemenu";
+            this.Pnl_Sidemenu.Size = new System.Drawing.Size(172, 638);
+            this.Pnl_Sidemenu.TabIndex = 1;
             // 
             // Btn_Customers
             // 
-            this.Btn_Customers.BackColor = System.Drawing.Color.Black;
+            this.Btn_Customers.BackColor = System.Drawing.Color.SteelBlue;
             this.Btn_Customers.FlatAppearance.BorderSize = 0;
+            this.Btn_Customers.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.Btn_Customers.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_Customers.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Customers.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Btn_Customers.ForeColor = System.Drawing.Color.Snow;
-            this.Btn_Customers.Location = new System.Drawing.Point(0, 317);
+            this.Btn_Customers.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Customers.Image")));
+            this.Btn_Customers.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_Customers.Location = new System.Drawing.Point(0, 367);
             this.Btn_Customers.Name = "Btn_Customers";
             this.Btn_Customers.Size = new System.Drawing.Size(180, 67);
             this.Btn_Customers.TabIndex = 4;
@@ -82,12 +89,15 @@
             // 
             // Btn_Storage
             // 
-            this.Btn_Storage.BackColor = System.Drawing.Color.Black;
+            this.Btn_Storage.BackColor = System.Drawing.Color.SteelBlue;
             this.Btn_Storage.FlatAppearance.BorderSize = 0;
+            this.Btn_Storage.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.Btn_Storage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_Storage.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Storage.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Btn_Storage.ForeColor = System.Drawing.Color.Snow;
-            this.Btn_Storage.Location = new System.Drawing.Point(0, 244);
+            this.Btn_Storage.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Storage.Image")));
+            this.Btn_Storage.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_Storage.Location = new System.Drawing.Point(0, 294);
             this.Btn_Storage.Name = "Btn_Storage";
             this.Btn_Storage.Size = new System.Drawing.Size(180, 67);
             this.Btn_Storage.TabIndex = 3;
@@ -97,12 +107,15 @@
             // 
             // Btn_Sale
             // 
-            this.Btn_Sale.BackColor = System.Drawing.Color.Black;
+            this.Btn_Sale.BackColor = System.Drawing.Color.SteelBlue;
             this.Btn_Sale.FlatAppearance.BorderSize = 0;
+            this.Btn_Sale.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.Btn_Sale.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_Sale.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Sale.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Btn_Sale.ForeColor = System.Drawing.Color.Snow;
-            this.Btn_Sale.Location = new System.Drawing.Point(0, 171);
+            this.Btn_Sale.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Sale.Image")));
+            this.Btn_Sale.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_Sale.Location = new System.Drawing.Point(0, 221);
             this.Btn_Sale.Name = "Btn_Sale";
             this.Btn_Sale.Size = new System.Drawing.Size(180, 67);
             this.Btn_Sale.TabIndex = 2;
@@ -112,12 +125,15 @@
             // 
             // Btn_Employee
             // 
-            this.Btn_Employee.BackColor = System.Drawing.Color.Black;
+            this.Btn_Employee.BackColor = System.Drawing.Color.SteelBlue;
             this.Btn_Employee.FlatAppearance.BorderSize = 0;
+            this.Btn_Employee.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.Btn_Employee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_Employee.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Employee.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Btn_Employee.ForeColor = System.Drawing.Color.Snow;
-            this.Btn_Employee.Location = new System.Drawing.Point(0, 98);
+            this.Btn_Employee.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Employee.Image")));
+            this.Btn_Employee.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_Employee.Location = new System.Drawing.Point(-3, 148);
             this.Btn_Employee.Name = "Btn_Employee";
             this.Btn_Employee.Size = new System.Drawing.Size(180, 67);
             this.Btn_Employee.TabIndex = 1;
@@ -127,12 +143,15 @@
             // 
             // Btn_Overview
             // 
-            this.Btn_Overview.BackColor = System.Drawing.Color.Black;
+            this.Btn_Overview.BackColor = System.Drawing.Color.SteelBlue;
             this.Btn_Overview.FlatAppearance.BorderSize = 0;
+            this.Btn_Overview.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.Btn_Overview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_Overview.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Overview.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Btn_Overview.ForeColor = System.Drawing.Color.Snow;
-            this.Btn_Overview.Location = new System.Drawing.Point(0, 25);
+            this.Btn_Overview.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Overview.Image")));
+            this.Btn_Overview.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Btn_Overview.Location = new System.Drawing.Point(0, 75);
             this.Btn_Overview.Name = "Btn_Overview";
             this.Btn_Overview.Size = new System.Drawing.Size(180, 67);
             this.Btn_Overview.TabIndex = 0;
@@ -192,67 +211,78 @@
             this.uC_overview1.Size = new System.Drawing.Size(839, 566);
             this.uC_overview1.TabIndex = 0;
             // 
-            // panel1
+            // panel3
             // 
-            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1017, 74);
-            this.panel1.TabIndex = 0;
+            this.panel3.BackColor = System.Drawing.Color.Orange;
+            this.panel3.Controls.Add(this.Lb_Name);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(172, 74);
+            this.panel3.TabIndex = 1;
             // 
-            // label1
+            // Lb_Name
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.Lb_Name.AutoSize = true;
+            this.Lb_Name.BackColor = System.Drawing.Color.Orange;
+            this.Lb_Name.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Lb_Name.Font = new System.Drawing.Font("Monotype Corsiva", 39.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lb_Name.Location = new System.Drawing.Point(0, 0);
+            this.Lb_Name.Name = "Lb_Name";
+            this.Lb_Name.Size = new System.Drawing.Size(153, 64);
+            this.Lb_Name.TabIndex = 0;
+            this.Lb_Name.Text = "G7 Ph";
+            this.Lb_Name.Click += new System.EventHandler(this.label1_Click);
             // 
             // errorProvider1
             // 
             this.errorProvider1.ContainerControl = this;
             // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(172, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(845, 74);
+            this.panel1.TabIndex = 3;
+            // 
             // FrmMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(1017, 638);
-            this.Controls.Add(this.Pnl_Main);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Controls.Add(this.Pnl_Main);
+            this.Controls.Add(this.Pnl_Sidemenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "FrmMain";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FrmMain_Load);
-            this.panel2.ResumeLayout(false);
+            this.Pnl_Sidemenu.ResumeLayout(false);
             this.Pnl_Main.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel Pnl_Sidemenu;
         private System.Windows.Forms.Panel Pnl_Main;
         private System.Windows.Forms.Button Btn_Overview;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button Btn_Customers;
         private System.Windows.Forms.Button Btn_Storage;
         private System.Windows.Forms.Button Btn_Sale;
         private System.Windows.Forms.Button Btn_Employee;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lb_Name;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private UC_Customers uC_Customers1;
         private UC_Storage uC_Storage1;
         private UC_Sale uC_Sale1;
         private PharmacyManagement.UC_Employee uC_employee1;
         private PharmacyManagement.UC_Overview uC_overview1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- PharmacyManagement/UC_Overview.Designer.cs
+++ PharmacyManagement/UC_Overview.Designer.cs
@@ -33,10 +33,19 @@
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lb_Doanhso = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -50,24 +59,12 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(71, 49);
+            this.panel1.BackColor = System.Drawing.Color.MediumOrchid;
+            this.panel1.Controls.Add(this.lb_Doanhso);
+            this.panel1.Location = new System.Drawing.Point(30, 49);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(177, 76);
             this.panel1.TabIndex = 1;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(330, 49);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 100);
-            this.panel2.TabIndex = 2;
-            // 
-            // panel3
-            // 
-            this.panel3.Location = new System.Drawing.Point(588, 49);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(200, 100);
-            this.panel3.TabIndex = 3;
             // 
             // chart1
             // 
@@ -85,18 +82,93 @@
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(231, 49);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(177, 76);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightBlue;
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Location = new System.Drawing.Point(432, 49);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(177, 76);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.IndianRed;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Location = new System.Drawing.Point(632, 49);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(177, 76);
+            this.panel4.TabIndex = 2;
+            // 
+            // lb_Doanhso
+            // 
+            this.lb_Doanhso.AutoSize = true;
+            this.lb_Doanhso.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lb_Doanhso.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Doanhso.ForeColor = System.Drawing.Color.White;
+            this.lb_Doanhso.Location = new System.Drawing.Point(91, 0);
+            this.lb_Doanhso.Name = "lb_Doanhso";
+            this.lb_Doanhso.Size = new System.Drawing.Size(86, 22);
+            this.lb_Doanhso.TabIndex = 0;
+            this.lb_Doanhso.Text = "Doanh Số";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(71, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "label5";
+            // 
             // UC_Overview
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.chart1);
-            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label1);
             this.Name = "UC_Overview";
             this.Size = new System.Drawing.Size(839, 556);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,8 +178,13 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label lb_Doanhso;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
     }
 }
