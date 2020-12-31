9fde8747078e4f533362fe0676e750023fbed3b4
G-Schwarz <46079708+G-Schwarz@users.noreply.github.com>
2020-12-31 09:12:29 +0700

    Setting

--- PharmacyManagement/UC_Settings.cs
+++ PharmacyManagement/UC_Settings.cs
@@ -16,5 +16,28 @@ namespace PharmacyManagement
         {
             InitializeComponent();
         }
+
+        private void UC_Settings_Load(object sender, EventArgs e)
+        {
+            pnl_AccountManagement.Visible = true;
+            pnl_AddAcc.Visible = false;
+        }
+
+        private void btn_AccManagement_Click(object sender, EventArgs e)
+        {
+            pnl_AccountManagement.Visible = true;
+            pnl_AddAcc.Visible = false;
+        }
+
+        private void btn_AddAcc_Click(object sender, EventArgs e)
+        {
+            pnl_AccountManagement.Visible = false;
+            pnl_AddAcc.Visible = true;
+        }
+
+        private void btn_Logout_Click(object sender, EventArgs e)
+        {
+            FrmMain.Close();
+        }
     }
 }
